d225a2061ff7fb6f8a629deb15ae66a013832bed
Ibrahim-rana <122722135+Ibrahim-rana@users.noreply.github.com>
2023-12-17 20:50:23 +0300

    Add files via upload

---
+++ Board.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "Field.h"
+#include "GameObject.h"
+
+#include <list>
+
+class Board final : public GameObject
+{
+public:
+    Board(uint16_t sideFieldNumber);
+    virtual ~Board();
+
+    virtual void init() override;
+    virtual void update(sf::Time time) override;
+    virtual void draw(sf::RenderTarget &renderTarget, sf::RenderStates states) const override;
+
+    void setFontScaleFactor(float scale) override;
+
+    std::list<Field *> getFieldsByColor(Field::Color color) const;
+    Field *getFieldByPosition(const sf::Vector2f &position) const;
+    Field *getFieldByBoardPosition(const sf::Vector2i &position) const;
+    Field *getFieldByName(const std::string &name) const;
+
+    void clearSelection();
+
+    bool isBoardPositionValid(const sf::Vector2i &position) const;
+    bool isBoardPositionInvalid(const sf::Vector2i &position) const;
+    bool isBoardPositionOccupied(const sf::Vector2i &position) const;
+
+private:
+    uint16_t sideFieldsNumber;
+    sf::ConvexShape boardShape;
+    Field ***fields;
+    sf::IntRect validFields;
+    float fieldMargin;
+};
